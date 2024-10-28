8800be5edd928182d5eb02fa9827bea132dd1f44
Clifong <59185915+Clifong@users.noreply.github.com>
2024-10-28 21:09:49 +0800

    Fied authentication, now works
    Fixed authentication now works

--- src/app/pages/auth/register/page.tsx
+++ src/app/pages/auth/register/page.tsx
@@ -1,16 +1,25 @@
+"use client";
+import registerUser from "@/app/databse/firebase";
+import firebaseApp from "@/app/databse/firebase";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form action={ registerUser } className="space-y-6">
           <label className="block text-sm font-medium leading-6 text-gray-900">
             Email address:
           </label>
 
           <div className="mt-2">
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="email"
               name="email"
@@ -26,6 +35,8 @@ export default function LoginPage() {
           </div>
           <div className="mt-2">
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="password"
               name="password"
@@ -38,13 +49,13 @@ export default function LoginPage() {
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Login
+            Register
           </button>
         </form>
 
-        <p className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+        <div className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
           <Link href="/pages/auth/login">Already a member?</Link>
-        </p>
+        </div>
       </div>
     </div>
   );

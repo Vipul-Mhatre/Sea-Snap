49cdf20bfcafb9ba9d1a7090f3df44e8f8ad5475
Clifong <59185915+Clifong@users.noreply.github.com>
2024-10-29 23:32:25 +0800

    Improved authenticatoion
    Improved authenticatoion

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -1,17 +1,33 @@
 "use client";
-import registerUser from "@/app/databse/firebase";
-import firebaseApp from "@/app/databse/firebase";
 import Link from "next/link";
-import { useState } from "react";
+import database from "@/app/databse/firebase"
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/router";
+import { permanentRedirect } from "next/navigation";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState<string | null>("")
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    try {
+      await database.login(email, password)
+    } catch (e) {
+      setError(e.message)
+      setEmail("")
+      setPassword("")
+      return
+    }
+    permanentRedirect("/")
+  }
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action={ registerUser } className="space-y-6">
+        <form onSubmit={ onSubmit } className="space-y-6">
           <label className="block text-sm font-medium leading-6 text-gray-900">
             Email address:
           </label>
@@ -35,9 +51,9 @@ export default function LoginPage() {
           </div>
           <div className="mt-2">
             <input
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="password"
               name="password"
               placeholder="Password"
@@ -49,13 +65,16 @@ export default function LoginPage() {
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Register
+            Login
           </button>
+          <div style={{color: "red"}}>
+            {error}
+          </div>
         </form>
 
-        <div className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-          <Link href="/pages/auth/login">Already a member?</Link>
-        </div>
+        <p className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          <Link href="/auth/signup">Not a member?</Link>
+        </p>
       </div>
     </div>
   );
--- src/app/(pages)/auth/signup/page.tsx
+++ src/app/(pages)/auth/signup/page.tsx
@@ -1,16 +1,39 @@
+"use client";
+import database from "@/app/databse/firebase";
 import Link from "next/link";
+import { permanentRedirect } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>("")
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    try {
+      await database.registerUser(email, password)
+    } catch (e) {
+      setError(e.message)
+      setEmail("")
+      setPassword("")
+      return
+    }
+    permanentRedirect("/")
+  }
+  
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form onSubmit={onSubmit} className="space-y-6">
           <label className="block text-sm font-medium leading-6 text-gray-900">
             Email address:
           </label>
 
           <div className="mt-2">
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="email"
               name="email"
@@ -26,6 +49,8 @@ export default function LoginPage() {
           </div>
           <div className="mt-2">
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               type="password"
               name="password"
@@ -38,13 +63,16 @@ export default function LoginPage() {
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Login
+            Register
           </button>
+          <div style={{color: "red"}}>
+            {error}
+          </div>
         </form>
 
-        <p className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-          <Link href="/pages/auth/register">Not a member?</Link>
-        </p>
+        <div className="mt-10 text-center text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          <Link href="/auth/login">Already a member?</Link>
+        </div>
       </div>
     </div>
   );
